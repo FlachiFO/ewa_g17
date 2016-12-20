4a0b1f881bcc4b27c36b82f257d2e39e02be0fac
FlachiFO <b.flache@felgenoutlet.de>
2016-12-20 11:26:01 +0100

    -> Registrung ist nun eingebunden!

---
+++ config/regist_process.php
@@ -0,0 +1,65 @@
+<?php
+	session_start();
+	require_once 'db_config.php';
+
+	if(isset($_POST['btn-regist']))
+	{
+		$user = trim($_POST['user']);
+		$user_password = trim($_POST['password']);
+        $user_confirm_password = trim($_POST['confirm_password']);
+        $address = trim($_POST['address']);
+	
+		$password = $user_password;
+        
+        if($user_password == $user_confirm_password) {
+        
+            if($user == "" OR $user_password == "" OR $user_confirm_password == "" OR $address == "")
+            {
+                echo "Eingabefehler. Bitte alle Felder ausfüllen!";
+                exit;
+            }
+
+            $sql="SELECT Username FROM user WHERE Username='$user'";
+            $result = mysqli_query($db_con,$sql);
+
+            if (mysqli_fetch_row($result) > 0) {    
+                echo 'Benutzername schon vorhanden!';  
+            } 
+            else {
+                $regist = "INSERT INTO user (Username, Userpwmd5, UserAdresse) VALUES ('$user', '$password', '$address')";
+                $eintragen = mysqli_query($db_con,$regist);
+                if($eintragen == true)
+                    {
+                       $sql="SELECT UserID, Username FROM user WHERE Username='$user' AND Userpwmd5='$password'";
+                        $result = mysqli_query($db_con,$sql);
+
+                        $row = mysqli_fetch_row($result);
+
+                        $menge = mysqli_num_rows($result);
+
+                        if($menge == 1)
+                        {
+                            //session_register($username);
+                            $_SESSION['user_session_id']=$row[0];
+                            $_SESSION['user_session_name']=$row[1];
+                            echo "ok"; // log in
+
+                        }
+                        else
+                        {
+                             echo "Login nicht erfolgreich";
+                         }
+
+                    }
+                else
+                    {
+                    echo 'Fehler beim Speichern des Kontos.';
+                    }
+            }
+        }
+        else {
+            echo "Passwörter stimmen nicht überein!";
+        }
+	}
+
+?>
--- js/function.js
+++ js/function.js
@@ -1,62 +1,99 @@
-$('document').ready(function()
-{ 
-     /* validation */
-	 $("#login-form").validate({
-      rules:
-	  {
-			password: {
-			required: true,
-			},
-			user_email: {
-            required: true,
-            user_name: true
-            },
-	   },
-       messages:
-	   {
-            password:{
-                      required: "please enter your password"
-                     },
-            user: "please enter your username",
-       },
-	   submitHandler: submitForm	
-       });  
-	   /* validation */
-	   
-	   /* login submit */
-	   function submitForm()
-	   {		
-			var data = $("#login-form").serialize();
-				
-			$.ajax({
-				
-			type : 'POST',
-			url  : './config/login_process.php',
-			data : data,
-			beforeSend: function()
-			{	
-				$("#error").fadeOut();
-//				$("#btn-login").html('<span class="glyphicon glyphicon-transfer"></span> &nbsp; sending ...');
-			},
-			success :  function(response)
-			   {						
-					if(response=="ok"){
-									
-						$("#btn-login").html('<img class="load_img" src="./img/btn-ajax-loader.gif" /> &nbsp; Signing In ...');
-//						setTimeout(' window.location.href = "./home.php"; ',4000);
-                         window.location.href = "./home.php";
-					}
-					else{
-									
-						$("#error").fadeIn(1000, function(){						
-				$("#error").html('<div class="alert alert-danger"> <span class="glyphicon glyphicon-info-sign"></span> &nbsp; '+response+' !</div>');
-											$("#btn-login").html('<span class="glyphicon glyphicon-log-in"></span> &nbsp; Sign In');
-									});
-					}
-			  }
-			});
-				return false;
-		}
-	   /* login submit */
+$('document').ready(function () {
+    /* validation */
+    $("#login-form").validate({
+        rules: {
+            password: {
+                required: true
+            , }
+            //            , user_email: {
+            //                required: true
+            //                , user_name: true
+            //            }
+            
+        , }
+        , messages: {
+            password: {
+                required: "please enter your password"
+            }
+            , user: "please enter your username"
+        , }
+        , submitHandler: submitForm
+    });
+    /* validation */
+    /* login submit */
+    function submitForm() {
+        var data = $("#login-form").serialize();
+        $.ajax({
+            type: 'POST'
+            , url: './config/login_process.php'
+            , data: data
+            , beforeSend: function () {
+                $("#error").fadeOut();
+                //				$("#btn-login").html('<span class="glyphicon glyphicon-transfer"></span> &nbsp; sending ...');
+            }
+            , success: function (response) {
+                if (response == "ok") {
+//                    $("#btn-login").html('<img class="load_img" src="./img/btn-ajax-loader.gif" /> &nbsp; Signing In ...');
+                    //						setTimeout(' window.location.href = "./home.php"; ',4000);
+                    window.location.href = "./home.php";
+                }
+                else {
+                    $("#error").fadeIn(1000, function () {
+                        $("#error").html('<div class="alert alert-danger"> <span class="glyphicon glyphicon-info-sign"></span> &nbsp; ' + response + ' !</div>');
+//                        $("#btn-login").html('<span class="glyphicon glyphicon-log-in"></span> &nbsp; Sign In');
+                    });
+                }
+            }
+        });
+        return false;
+    }
+    $("#register-form").validate({
+        rules: {
+            password: {
+                required: true
+            , }
+            //            , user_email: {
+            //                required: true
+            //                , user_name: true
+            //            }
+            
+        , }
+        , messages: {
+            password: {
+                required: "please enter your password"
+            }
+            , user: "please enter your username"
+        , }
+        , submitHandler: submitForm
+    });
+    /* validation */
+    /* login submit */
+    function submitForm() {
+        var data = $("#register-form").serialize();
+        $.ajax({
+            type: 'POST'
+            , url: './config/regist_process.php'
+            , data: data
+            , beforeSend: function () {
+                $("#error").fadeOut();
+                //				$("#btn-login").html('<span class="glyphicon glyphicon-transfer"></span> &nbsp; sending ...');
+            }
+            , success: function (response) {
+                if (response == "ok") {
+//                    $("#btn-regist").html('<img class="load_img" src="./img/btn-ajax-loader.gif" /> &nbsp; Signing In ...');
+                    $("#error2").html('<div class="alert alert-success"> <span class="glyphicon glyphicon-info-sign"></span> &nbsp; Registrierung erfolgreich!</div>');
+                    setTimeout(' window.location.href = "./home.php"; ',4000);
+//                    window.location.href = "./home.php";
+                }
+                else {
+                    $("#error2").fadeIn(1000, function () {
+                        $("#error2").html('<div class="alert alert-danger"> <span class="glyphicon glyphicon-info-sign"></span> &nbsp; ' + response + ' !</div>');
+//                        $("#btn-regist").html('<span class="glyphicon glyphicon-log-in"></span> &nbsp; Sign In');
+                    });
+                }
+            }
+        });
+        return false;
+    }
+    /* login submit */
 });
-     
